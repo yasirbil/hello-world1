a2a8e2ec930f303cbbf92f28e369242ba2d7af7e
yasirbil <yasirbil@gmail.com>
2021-03-24 04:31:08 +0000

    hello-world1 changes

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,9 +1,9 @@
 module "academy-deploy" {
   source  = "fuchicorp/chart/helm"
-  deployment_name        = "hello-world"
+  deployment_name        = "hello-world1"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
-  deployment_path        = "hello-world"
+  deployment_path        = "hello-world1"
   template_custom_vars  = {     
     deployment_image     = "${var.deployment_image}"
   }
