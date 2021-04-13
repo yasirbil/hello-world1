9e270086b31a61022dd7715edc0af054481bb5c0
Yasir Bilgin <51059377+yasirbil@users.noreply.github.com>
2021-04-12 20:19:39 -0500

    Update app.py

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
+        'message': 'hello new world',
         'environment': os.environ.get('ENVIRONMENT')
     })
 if __name__ == '__main__':
